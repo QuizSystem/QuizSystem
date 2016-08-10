7bcfa51981f4b64929f9851d4bbc972e9100db89
framgia <framgia@framgias-Mac-mini-5.local>
2016-08-10 19:13:28 +0700

    1 - Init Project

---
+++ QuizSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizSystem
+//
+//  Created by framgia on 8/10/16.
+//  Copyright © 2016 QuizSystem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
